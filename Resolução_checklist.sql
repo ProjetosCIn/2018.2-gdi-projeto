186b4e1c79407746a383e92641506c2cbc122da7
SenhorN <danielgrupomh@gmail.com>
2018-10-08 12:59:19 -0300

    Update Resolução_checklist.sql

--- Resolução_checklist.sql
+++ Resolução_checklist.sql
@@ -1,6 +1,22 @@
 
+--15) Adiciona uma coluna na tabela 'poder'
+ALTER TABLE poder
+ADD nivel_de_forca NUMBER; 
+
+--16) Exclui a coluna nivel_de_forca da tabela 'poder'.
+
+ALTER TABLE poder
+DROP COLUMN nivel_de_forca;
+
+
+
+
+
+
 --20) Caso um valor esteja duplicado, retorna ele apenas uma vez
 
 SELECT DISTINCT endereço_de_nascimento
 FROM personagem
+
+
 
