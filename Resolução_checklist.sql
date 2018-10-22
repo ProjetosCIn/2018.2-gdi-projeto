133470a01f8cbaad61fcb4f39b2d9d8382dd8493
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-10-22 10:45:39 -0300

    Queries de checklist_2 colocados no arquivo de checklist principal

--- Resolução_checklist.sql
+++ Resolução_checklist.sql
@@ -1,5 +1,43 @@
+--1) Retorna todos as equipes com o determinado numero de fãs
+SELECT codigo_de_equipe
+FROM equipe                         
+WHERE n_de_fãs BETWEEN 1 AND 999999
 
+--2) Retorna todos os personagems com a data de nascimento no intervalo especificado
+SELECT codinome
+FROM personagem
+WHERE data_de_nascimento BETWEEN to_date('1996-07-01', 'yyyy-mm-dd') AND to_date('2000-07-31', 'yyyy-mm-dd')
 
+--3) Retorna todos os personagens que tem 'Might' no nome
+SELECT codinome
+FROM personagem 
+WHERE codinome LIKE '%Might%'
+
+--4) Retorna todos os heróis
+SELECT *
+FROM personagem
+WHERE codinome IN (SELECT codinome FROM heroi)
+
+--5) Retorna as frases de efeito de todos os heróis que tem CRH
+SELECT frase_de_efeito
+FROM personagem
+WHERE codinome IN (SELECT codinome FROM heroi WHERE crh IS NOT NULL)
+
+--6) Retorna os personagens ordenados pela data de nascimento
+SELECT *
+FROM personagem
+ORDER BY data_de_nascimento ASC;
+
+--7) 
+CREATE VIEW MATERNIDADES AS
+SELECT endereço, latitude, longitude
+FROM lugar, (SELECT endereço_de_nascimento FROM personagem) aux
+WHERE lugar.endereço = aux.endereço_de_nascimento
+
+--8) 
+SELECT *
+FROM MATERNIDADES
+GROUP BY endereço, latitude, longitude
 
 --14) Modifica o tipo da coluna n_de_fãs
 ALTER TABLE equipe
@@ -147,6 +185,17 @@ MINUS
 
 SELECT * FROM personagem p1
 WHERE p1.uniforme = 'Emana desespero'
+
+--42)
+SELECT endereço, latitude, longitude
+FROM lugar, (SELECT endereço_de_nascimento FROM personagem)aux
+WHERE lugar.endereço = aux.endereço_de_nascimento
+GROUP BY endereço, latitude, longitude
+
+--44) RETORNA TODOS OS NOMES DE HERÓIS COM DATA DE NASCIMENTO MÍNIMA ATÉ O NUMERO MÉDIO
+SELECT codinome
+FROM heroi
+WHERE crh MIN(data_de_nascimento) AND AVG(data_de_nascimento)
 
 
 --45) junçao de tres tabelas utilizando inner join
