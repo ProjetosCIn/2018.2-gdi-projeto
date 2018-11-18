45dc323d3320db918e5d367b3d972ce1e0c532bf
SenhorN <danielgrupomh@gmail.com>
2018-11-18 20:25:15 -0300

    Update Criação_OR.sql

--- Criação_OR.sql
+++ Criação_OR.sql
@@ -25,11 +25,10 @@ CREATE OR REPLACE TYPE tp_Personagem AS OBJECT(
     Data_de_Nascimento DATE,
     REF_equipe REF tp_Equipe,
     Mentor REF tp_Personagem,
-    Mentorando REF tp_Personagem
-    
-    
+    Mentorando REF tp_Personagem    
 )NOT FINAL NOT INSTANTIABLE; -- abstrato
 /
+
 CREATE OR REPLACE TYPE tp_Poder AS OBJECT(
     Cod_Poder NUMBER(10),
     Nome VARCHAR2(50),
@@ -37,27 +36,27 @@ CREATE OR REPLACE TYPE tp_Poder AS OBJECT(
     Legalidade VARCHAR2(6) -- LEGAL OU ILEGAL
 );
 /
+
 CREATE OR REPLACE TYPE tp_Heroi UNDER tp_Personagem(
     Crh NUMBER(10), 
     REF_poder REF tp_Poder
 )FINAL;
 /
+
 CREATE OR REPLACE TYPE tp_Vilão UNDER tp_Personagem(
     Numero_De_Procurado NUMBER(10),
     REF_poder REF tp_Poder
 )FINAL;
 /
 
-
 CREATE OR REPLACE TYPE tp_Area AS OBJECT(
     Area VARCHAR2(255),
     REF_Equipe REF tp_Equipe
-
 );
 /
+
 CREATE OR REPLACE TYPE Area_De_Atuação AS TABLE OF tp_Area; 
 /
-
 
 
 CREATE OR REPLACE TYPE tp_Qg AS OBJECT(
@@ -68,16 +67,16 @@ CREATE OR REPLACE TYPE tp_Qg AS OBJECT(
 
 );
 /
+
 CREATE OR REPLACE TYPE tp_Símbolo AS OBJECT(
     
     Cor_Predominante VARCHAR2(30),
     Significado VARCHAR2(255),
     Id_Imagem NUMBER,
     REF_equipe REF tp_Equipe
-   
-
 );
 /
+
 CREATE OR REPLACE TYPE tp_Luta AS OBJECT(
     Data DATE,
     Endereço tp_Lugar,
@@ -89,7 +88,6 @@ CREATE OR REPLACE TYPE tp_Luta AS OBJECT(
 
 CREATE TABLE tb_Lugar OF tp_Lugar (
     PRIMARY KEY (Endereço)
-
 );
 
 CREATE TABLE tb_Equipe OF tp_Equipe(
@@ -104,3 +102,53 @@ CREATE TABLE tb_Poder OF tp_Poder(
     PRIMARY KEY (Cod_Poder)
 );
 
+--Criação das tabelas
+DROP TABLE tb_Lugar;
+DROP TABLE tb_Equipe;
+DROP TABLE tb_Personagem;
+DROP TABLE tb_Poder;
+DROP TABLE tb_Heroi;
+DROP TABLE tb_Vilão;
+DROP TABLE tb_Qg;
+DROP TABLE tb_Símbolo;
+DROP TABLE tb_Luta;
+
+
+CREATE TABLE tb_Lugar OF tp_Lugar (
+    PRIMARY KEY (Endereço)
+);
+
+CREATE TABLE tb_Equipe OF tp_Equipe(
+    PRIMARY KEY (Cod_De_Equipe)
+);
+
+CREATE TABLE tb_Personagem OF tp_Personagem(
+    PRIMARY KEY (Codinome)
+);
+
+CREATE TABLE tb_Poder OF tp_Poder(
+    PRIMARY KEY (Cod_Poder)
+);
+
+CREATE TABLE tb_Heroi OF tp_Heroi(
+    PRIMARY KEY (Codinome)
+    FOREIGN KEY(REF_Heroi)
+);
+
+CREATE TABLE tb_Vilão OF tp_Vilão( 
+    PRIMARY KEY (Numero_De_Procurado)
+);
+
+CREATE TABLE tb_Qg OF tp_Qg( -- ERRO , ta dando invalid indentifier
+    PRIMARY KEY (Numero_De_Qg,Cod_De_Equipe),
+    FOREIGN KEY(REF_Equipe) REFERENCES tb_Equipe    
+);
+
+CREATE TABLE tb_Símbolo OF tp_Símbolo(
+    PRIMARY KEY (Id_Imagem)
+);
+
+CREATE TABLE tb_Luta OF tp_Luta(--???
+    PRIMARY KEY () -- CodinomeHeroi, CodinomeVilao,Endereço_luta,Data_luta
+
+);
